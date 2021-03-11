268d8026577f30ed49b29fb06be472fc9c152183
Oleg <sunskays@gmail.com>
2021-03-11 14:35:51 +0200

    Example

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = ({title,history}) => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>{title} </p>
+    </div>
+  );
+};
+
+export default Home;
